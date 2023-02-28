b448e0cc2b7e76fe0e4f1f97a3a115e4f7f337fe
Yawny_laptop <yawnyyboy@gmail.com>
2023-02-28 21:44:22 +0100

    More game manager stuff

--- Assets/_scripts/Editor/GameManagerWindow.cs
+++ Assets/_scripts/Editor/GameManagerWindow.cs
@@ -3,8 +3,16 @@ using UnityEngine;
 
 public class GameManagerWindow : EditorWindow
 {
-    public FloatVariable variable;
-    private GameManager _managerScript;
+    public FloatVariable EnemyRows;
+    public FloatVariable EnemyColumns;
+    public FloatVariable Score;
+
+
+    GameManager _managerScript;
+
+    bool _showEnemyStuff = false;
+    bool _showPlayerStuff = false;
+    bool _showGameStuff = false;
 
     [MenuItem("Window/GameManager")]
     static void Init()
@@ -13,46 +21,88 @@ public class GameManagerWindow : EditorWindow
         window.Show();
     }
 
-    private void OnGUI()
+    private void OnEnable()
     {
         _managerScript = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
-
-        ScoreField();
-
-        EditorGUILayout.Space();
-        EditorGUILayout.Space();
-
-        GUILayout.BeginHorizontal();
-        if (GUILayout.Button("Pause Game"))
+    }
+    private void OnGUI()
+    {
+        _showPlayerStuff = EditorGUILayout.Foldout(_showPlayerStuff, "Player Stuff");
+        if (_showPlayerStuff)
         {
-            _managerScript.Pause();
+
         }
-        if(GUILayout.Button("Resume Game"))
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        _showEnemyStuff = EditorGUILayout.Foldout(_showEnemyStuff, "Enemy Stuff");
+        if (_showEnemyStuff)
         {
-            _managerScript.Resume();
+            EnemyRowAndColCount();
+            if (GUILayout.Button("Reset Enemies"))
+            {
+                _managerScript.ResetEnemies();
+            }
         }
-        GUILayout.EndHorizontal();
 
         EditorGUILayout.Space();
         EditorGUILayout.Space();
 
-        if (GUILayout.Button("Reset Enemies"))
+        _showGameStuff = EditorGUILayout.Foldout(_showGameStuff, "Game Stuff");
+        if (_showGameStuff)
         {
-            _managerScript.ResetEnemies();
+            ScoreField();
+            PlaybackButtons();
         }
     }
 
     private void ScoreField()
     {
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Score");
-        int scoreField = EditorGUILayout.IntField((int)variable.Value);
+        EditorGUILayout.BeginHorizontal();
+        Label("Score");
+        int scoreField = EditorGUILayout.IntField((int)Score.Value);
         if (GUILayout.Button("Update Score"))
         {
             _managerScript.UpdateScoreUI();
         }
-        variable.SetValue(scoreField);
+        Score.SetValue(scoreField);
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void EnemyRowAndColCount()
+    {
+        EditorGUILayout.BeginHorizontal();
+        Label("Rows");
+        int rows = EditorGUILayout.IntField((int)EnemyRows.Value);
+        Score.SetValue(rows);
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+        Label("Columns");
+        int columns = EditorGUILayout.IntField((int)EnemyRows.Value);
+        Score.SetValue(columns);
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void PlaybackButtons()
+    {
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Pause Game"))
+        {
+            _managerScript.Pause();
+        }
+        if (GUILayout.Button("Resume Game"))
+        {
+            _managerScript.Resume();
+        }
         GUILayout.EndHorizontal();
     }
 
+    private void Label(string str)
+    {
+        EditorGUILayout.LabelField(str, GUILayout.Width(80));
+    }
 }
