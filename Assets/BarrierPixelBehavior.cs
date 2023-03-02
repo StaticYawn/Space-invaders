d3e297ae2abf26126216935d484a58d9f66ce5dc
Yawny_laptop <yawnyyboy@gmail.com>
2023-03-02 14:14:33 +0100

    BARRIERS DONE

---
+++ Assets/BarrierPixelBehavior.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierPixelBehavior : MonoBehaviour
+{
+
+    float _radius = 0.0625f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        InnerCircleCast();
+        OutterCircleCast();
+        Destroy(gameObject);
+    }
+
+    private void InnerCircleCast()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, _radius, new Vector2(0,0));
+        foreach(RaycastHit2D hit in hits)
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+
+    private void OutterCircleCast()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, _radius * 3, new Vector2(0, 0));
+        foreach (RaycastHit2D hit in hits)
+        {
+            float rnd = Random.Range(0, 15);
+            if(rnd > 15 /2)
+            {
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawSphere(transform.position, _radius);
+    }
+}
---
+++ Assets/BarrierSetup.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrierSetup : MonoBehaviour
+{
+    [SerializeField] Texture2D _barrierSprite;
+    [SerializeField] GameObject _barrierPixel;
+
+    int _width;
+    int _height;
+
+    float _pixelUnit = 0.0625f;
+
+    int _widthHalf;
+    void Start()
+    {
+        _width = _barrierSprite.width;
+        _widthHalf = _width / 2;
+        _height = _barrierSprite.height;
+
+        Setup();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void Setup()
+    {
+        for(int x = 0; x < _width; x++)
+        {
+            for(int y = 0; y < _height; y++)
+            {
+                Color pixelColor = _barrierSprite.GetPixel((int)x, y);
+                if (pixelColor.a < 1) continue;
+                GameObject pixel = Instantiate(_barrierPixel);
+                pixel.transform.SetParent(transform);
+
+                Vector2 pos = new Vector2(transform.position.x + (x - _widthHalf) * _pixelUnit, transform.position.y + y * _pixelUnit);
+                pixel.transform.position = pos;
+            }
+        }
+    }
+}
